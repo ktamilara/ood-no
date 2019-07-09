ed0e2b25bcd60bfcfde9e4288c5307923a9b9eb9
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-07-09 15:29:23 +0530

    Create 131.py

---
+++ 131.py
@@ -0,0 +1,13 @@
+mm1=int(input())
+nn1=list(map(int,input().split()))
+temp=[]
+while(len(nn1)!=0):
+  if len(nn1)>1:
+    temp.append(max(nn1))
+    temp.append(min(nn1))
+    nn1.remove(max(nn1))
+    nn1.remove(min(nn1))
+  else:
+    temp.append(max(nn1))
+    nn1.remove(max(nn1))
+print(*temp,end="")  
