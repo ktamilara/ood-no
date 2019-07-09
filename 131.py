d7c404361e673320fd47e8fb3909126624cdfee7
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-07-09 15:30:55 +0530

    Update 131.py

--- 131.py
+++ 131.py
@@ -1,13 +1,13 @@
-mm1=int(input())
-nn1=list(map(int,input().split()))
+mm11=int(input())
+nn11=list(map(int,input().split()))
 temp=[]
-while(len(nn1)!=0):
-  if len(nn1)>1:
-    temp.append(max(nn1))
-    temp.append(min(nn1))
-    nn1.remove(max(nn1))
-    nn1.remove(min(nn1))
+while(len(nn11)!=0):
+  if len(nn11)>1:
+    temp.append(max(nn11))
+    temp.append(min(nn11))
+    nn11.remove(max(nn11))
+    nn11.remove(min(nn11))
   else:
-    temp.append(max(nn1))
-    nn1.remove(max(nn1))
+    temp.append(max(nn11))
+    nn11.remove(max(nn11))
 print(*temp,end="")  
