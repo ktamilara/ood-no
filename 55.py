a692f56a0f3f6e086117516dd8cb1338f760809a
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-07-09 16:28:31 +0530

    Update 55.py

--- 55.py
+++ 55.py
@@ -1,21 +1,21 @@
-nnum1,knum1 = map(int,input().split())
-u1 = list(map(int,input().split()))
-v1,w1 = 0,[]
-for i in range(0,len(u1)):
-  t1 = i
-  for a in range(0,len(u1)):
-    for l in range(0,knum1):
-      if l == knum1-1:
+n11,k1 = map(int,input().split())
+a11 = list(map(int,input().split()))
+b1,c1 = 0,[]
+for i in range(0,len(a11)):
+  t1 = i1
+  for p1 in range(0,len(a11)):
+    for l1 in range(0,k1):
+      if l1 == k1-1:
         try:
-          v1 += u1[a1+i]
+          b1 += a11[p1+i1]
         except IndexError:
             t1 = t1-1
-            v1 +=u1[t1]
+            b1 +=a11[t1]
       else:
-        v 1+= u1[i]
-    w1.append(v1)
-    v1 = 0
-for i in range(0,len(u1),knum1):
-  v1 = sum(u1[i:i+knum1])
-  w1.append(v1)
-print(*sorted(set(w1)))
+        b1 += a11[i1]
+    c1.append(b1)
+    b1 = 0
+for i in range(0,len(a11),k1):
+  b1 = sum(a11[i:i+k1])
+  c1.append(b1)
+print(*sorted(set(c1)))
