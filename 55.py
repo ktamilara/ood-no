c17c59e732f6be947155f3995aa5c7269c3806cc
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-07-09 16:31:58 +0530

    Update 55.py

--- 55.py
+++ 55.py
@@ -1,21 +1,21 @@
-n11,k1 = map(int,input().split())
-a11 = list(map(int,input().split()))
+nn1,kk1 = map(int,input().split())
+a1 = list(map(int,input().split()))
 b1,c1 = 0,[]
-for i in range(0,len(a11)):
-  t1 = i1
-  for p1 in range(0,len(a11)):
-    for l1 in range(0,k1):
-      if l1 == k1-1:
+for i in range(0,len(a1)):
+  t1 = i
+  for p1 in range(0,len(a1)):
+    for l1 in range(0,kk1):
+      if l1 == kk1-1:
         try:
-          b1 += a11[p1+i1]
+          b1 += a1[p1+i]
         except IndexError:
             t1 = t1-1
-            b1 +=a11[t1]
+            b1 +=a1[t1]
       else:
-        b1 += a11[i1]
+        b1 += a1[i]
     c1.append(b1)
     b1 = 0
-for i in range(0,len(a11),k1):
-  b1 = sum(a11[i:i+k1])
+for i in range(0,len(a1),kk1):
+  b1 = sum(a1[i:i+kk1])
   c1.append(b1)
 print(*sorted(set(c1)))
