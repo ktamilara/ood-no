51a579b3a52f42bd3c79f898ced592d5c20f1e8e
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-07-09 16:21:51 +0530

    Update 57.py

--- 57.py
+++ 57.py
@@ -1,10 +1,10 @@
-aa1,bb1=map(str,input().split())
-count1=0
-for i in range(0,len(aa1)):
-    for j in range(0,len(bb1)):
-        if aa[i]==bb1[j]:
-            coun1t+=1
-if count1>=2:
+number11,number12=map(str,input().split())
+countvalue1=0
+for i in range(0,len(number11)):
+    for j in range(0,len(number12)):
+        if number11[i]==number12[j]:
+            countvalue1+=1
+if countvalue1>=2:
     print("yes")
 else:
     print("no")
